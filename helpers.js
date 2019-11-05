ab2a5c768d6d58e8a56bc714b06044d055da70b4
Alfredo Fuentes <alfredofuentes@Alfredof.local>
2019-11-05 13:30:56 -0500

    cambios nuevos experimentales

--- helpers.js
+++ helpers.js
@@ -18,12 +18,12 @@ exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/static
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-exports.siteName = `Now That's Delicious!`;
+exports.siteName = `La Victoria Lab`;
 
 exports.menu = [
-  { slug: '/stores', title: 'Providers', icon: 'store', },
-  { slug: '/tags', title: 'Tags', icon: 'tag', },
-  { slug: '/top', title: 'Top', icon: 'top', },
+  { slug: '/stores', title: 'Proveedores', icon: 'store', },
+//  { slug: '/tags', title: 'Tags', icon: 'tag', },
+  { slug: '/top', title: 'Mas usados', icon: 'top', },
   { slug: '/add', title: 'Add', icon: 'add', },
   { slug: '/map', title: 'Map', icon: 'map', },
 ];
