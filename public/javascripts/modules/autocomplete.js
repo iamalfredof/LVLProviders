e759c02a96c9bc10b05c12d5834c49f75990dad3
Alfredo Fuentes <alfredofuentes@Alfredof.local>
2019-12-16 16:58:21 -0500

    arreglado problema de maps

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -4,6 +4,7 @@ function autocomplete(input, latInput, lngInput) {
 
   dropdown.addListener('place_changed', () => {
     const place = dropdown.getPlace();
+    console.log (place);
     latInput.value = place.geometry.location.lat();
     lngInput.value = place.geometry.location.lng();
   });
